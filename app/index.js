0e8bc74e8965a2fd43c9d824577e3fddecd8641e
Artem Chugainov <archual.krista@gmail.com>
2019-02-07 11:15:08 +0600

    Add koa-router, create test routes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,58 @@
 const serverClose = require('./utilities/processEvents');
-const express = require('express'); 
-// const Koa = require('koa');
-const test = 1;
+// const express = require('express'); 
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+
+// Api
+const app = new Koa();
+const router = new KoaRouter();
+
+// Middleware.
+app.use(json());
+
+router.get('/', ctx => ctx.body = 'Hello Docker World');
+router.get('/health', ctx => {
+  // do app logic here to determine if app is truly healthy
+  // you should return 200 if healthy, and anything else will fail
+  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+  ctx.body = 'I am happy and healthy';
+});
+router.get('/documents', async (ctx, next) => {
+  if (db) {
+    ctx.body = await getDocumentsPromise('documents');
+    // Insert new doc.
+    db.collection('documents').insertOne({
+      name: 'test'
+    });
+  }
+  else {
+    ctx.response.status = 500;
+    ctx.response.body = { message: 'Waiting for connection to database' };
+  }
+});
+
+async function getDocumentsPromise(collection) {
+  return new Promise((resolve, reject) => {
+    db.collection(collection).find({}).toArray(function(err, result) {
+      if (err) {
+        reject(new Error(err));
+      }
+      else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+app.use(router.routes()).use(router.allowedMethods());
+
 const MongoClient = require('mongodb').MongoClient;
 
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DATABASE_NAME } = process.env;
 
 // Connection URL for mongo.
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}`;
-console.log(url);
 
 // Create a new MongoClient
 const client = new MongoClient(url);
@@ -25,41 +69,9 @@ setTimeout(() => {
   });
 }, 2000);
 
-// Api
-const app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hello Docker World\n');
-});
-
-app.get('/health', function(req, res) {
-  // do app logic here to determine if app is truly healthy
-  // you should return 200 if healthy, and anything else will fail
-  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
-  res.send('I am happy and healthy\n');
-});
-
-app.get('/documents', function(req, res, next) {
-  // might have not been connected just yet
-  if (db) {
-    db.collection('documents').find({}).toArray(function(err, docs) {
-      if (err) {
-        console.error(err);
-        next(new Error('Error while talking to database'));
-      } else {
-        res.json(docs);
-      }
-    });
-  } else {
-    next(new Error('Waiting for connection to database'));
-  }
-});
-
 const PORT = process.env.PORT || 8080;
-var server = app.listen(PORT, function() {
+var server = app.listen(PORT, () => {
   console.log(`Webserver is ready and listening on port ${PORT}`);
 });
 
 serverClose(server);
-
-module.exports = app;
